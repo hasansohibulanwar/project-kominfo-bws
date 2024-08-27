16bd4ea63a5e951402a19621bbdff0229e8ea471
hasan sohib <aresuma307@gmail.com>
2024-08-27 13:46:19 +0700

    maps

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -78,13 +78,14 @@
         <section id="hero" class="hero section">
             <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative"
                 data-aos="zoom-out">
-                <img src="assets/img/mapan.png" class="img-fluid animated" alt="">
+                <img src="assets/img/LogoPemkab2021.png" class="img-fluid animated" alt="" style="width: 30cm; height: 80%; object-fit: cover;">
                  <div class="container" data-aos="fade-up" data-aos-delay="50">
                     <div class="row justify-content-start">
                         <div class="col-lg-15">
                             <h2>Welcome to Day</h2>
-                            <p>Kami mengelola data komprehensif untuk semua pegawai kami secara efisien</p>
-                            <a href="/login" class="btn-get-started">Get Started</a>
+                            <p>Kami mengelola Informasi terkini untuk semua dengan secara efisien</p>
+                            <a href="/login" class="btn-get-started" style="background-color: black; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none;">Get Started</a>
+
 
                             <!-- Tombol akan mengarah ke halaman login -->
                         </div>
@@ -1154,81 +1155,24 @@
           <!-- Section Title -->
           <div class="container section-title" data-aos="fade-up">
             <h2>Contact</h2>
-            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+            <p><i class="bi bi-geo-alt"></i> Lokasi Dinas Komunikasi dan Informatika (DISKOMINFO) Kabupaten Bondowoso</p>
+
           </div><!-- End Section Title -->
-
           <div class="mb-5">
-            <iframe style="width: 100%; height: 400px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen=""></iframe>
-          </div><!-- End Google Maps -->
+            <iframe
+                style="width: 100%; height: 400px;"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.4723667784656!2d113.82165827497588!3d-7.924090667383413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6dd25dd81460d%3A0x3c6be134cb8361e0!2sDinas%20Komunikasi%20dan%20Informatika%20(%20DISKOMINFO%20)%20Kabupaten%20Bondowoso!5e0!3m2!1sen!2sid!4v1693236120742!5m2!1sen!2sid"
+                frameborder="0"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
 
-          <div class="container" data-aos="fade">
 
-            <div class="row gy-5 gx-lg-5">
-
-              <div class="col-lg-4">
-
-                <div class="info">
-                  <h3>Get in touch</h3>
-                  <p>Et id eius voluptates atque nihil voluptatem enim in tempore minima sit ad mollitia commodi minus.</p>
-
-                  <div class="info-item d-flex">
-                    <i class="bi bi-geo-alt flex-shrink-0"></i>
-                    <div>
-                      <h4>Location:</h4>
-                      <p>A108 Adam Street, New York, NY 535022</p>
-                    </div>
                   </div><!-- End Info Item -->
 
-                  <div class="info-item d-flex">
-                    <i class="bi bi-envelope flex-shrink-0"></i>
-                    <div>
-                      <h4>Email:</h4>
-                      <p>info@example.com</p>
-                    </div>
-                  </div><!-- End Info Item -->
-
-                  <div class="info-item d-flex">
-                    <i class="bi bi-phone flex-shrink-0"></i>
-                    <div>
-                      <h4>Call:</h4>
-                      <p>+1 5589 55488 55</p>
-                    </div>
-                  </div><!-- End Info Item -->
-
-                </div>
-
-              </div>
-
-              <div class="col-lg-8">
-                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                  <div class="row">
-                    <div class="col-md-6 form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
-                    </div>
-                    <div class="col-md-6 form-group mt-3 mt-md-0">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
-                    </div>
-                  </div>
-                  <div class="form-group mt-3">
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
-                  </div>
-                  <div class="form-group mt-3">
-                    <textarea class="form-control" name="message" placeholder="Message" required=""></textarea>
-                  </div>
-                  <div class="my-3">
-                    <div class="loading">Loading</div>
-                    <div class="error-message"></div>
-                    <div class="sent-message">Your message has been sent. Thank you!</div>
-                  </div>
-                  <div class="text-center"><button type="submit">Send Message</button></div>
-                </form>
-              </div><!-- End Contact Form -->
-
-            </div>
-
-          </div>
-
-        </section><!-- /Contact Section -->
+        <!-- /Contact Section -->
 
       </main>
 
